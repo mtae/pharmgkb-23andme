4a99d225d9e6a99d59adc3d2fc7d10d64e74e4d2
Caleb Helbling <caleb.helbling@gmail.com>
2017-10-14 01:29:07 -0400

    Switched to link based SNP detection

--- pharmdna.js
+++ pharmdna.js
@@ -1,74 +1,87 @@
-var addCellToRow = function(row, value) {
-    var newCell = document.createElement("td");
-    var text = document.createTextNode(value);
-    newCell.appendChild(text);
-    row.appendChild(newCell);
-};
-
-var parse23AndMe = function(contents) {
-    var lines = contents.replace(/^#.*/gm, "").replace("\r", "").split("\n").map(function(line) { return line.split("\t"); });
-    var rsidToGenotype = {};
-    for (var i = 0; i < lines.length; i++) {
-        var line = lines[i];
-        // Ignore empty lines
-        if (line.length > 1) {
-            rsidToGenotype[line[0]] = line[3].replace(/\W/g, "");
-        }  
-    }
-
-    return rsidToGenotype;
-}
-
-var onTableLoad = function(table) {
-    var rows = table.querySelectorAll("tr");
-    var header = rows[0];
-
-    // Add an upload button to add genetic data
-    var tableConfigContainer = document.querySelector(".table-config > div");
-    var uploadButton = document.createElement("input");
-    uploadButton.setAttribute("type", "file");
-    var flexSpace = tableConfigContainer.querySelector(".flex-space");
-    tableConfigContainer.insertBefore(uploadButton, flexSpace);
+(function() {
+    var processedLinks = new WeakSet();
 
     var showResults = function(rsidToGenotype) {
-        //addCellToRow(row[0], "Your genotype");
-        //addCellToRow(row[1], "");
-
-        for (var i = 2; i < rows.length; i++) {
-            var row = rows[i];
-            var rsidList = row.querySelector("td .comma-list").children;
-            rsidLinksList = Array.prototype.slice.call(rsidList).map(function (span) { return span.querySelector("a"); });
-            var genotypes = rsidLinksList.map(function (rsidLink) {
-                var rsid = rsidLink.innerText;
-                if (rsid in rsidToGenotype)  {
-                    return rsidToGenotype[rsid];
-                } else {
-                    return "N/A"
-                }});
-            addCellToRow(row, genotypes.join(","));
+        var snpLinks = document.querySelectorAll("a");
+        for (var i = 0; i < snpLinks.length; i++) {
+            var link = snpLinks[i];
+            if (!processedLinks.has(link)) {
+                processedLinks.add(link);
+                var snpName = link.innerText;
+                if (snpName in rsidToGenotype) {
+                    link.innerText += "(" + rsidToGenotype[snpName] + ")";
+                }
+            }
         }
     };
 
-    var onUpload = function(e1) {
-        var r = new FileReader();
-        r.onload = function(e2) {
-            var contents = e2.target.result;
-            var rsidToGenotype = parse23AndMe(contents);
-            showResults(rsidToGenotype);
-            browser.storage.local.set({"rsidToGenotype": rsidToGenotype});
-        };
-        r.readAsText(e1.target.files[0]);
-    };
-    uploadButton.addEventListener("input", onUpload, false);
+    var parse23AndMe = function(contents) {
+        var lines = contents.replace(/^#.*/gm, "").replace("\r", "").split("\n").map(function(line) { return line.split("\t"); });
+        var rsidToGenotype = {};
+        for (var i = 0; i < lines.length; i++) {
+            var line = lines[i];
+            // Ignore empty lines
+            if (line.length > 1) {
+                rsidToGenotype[line[0]] = line[3].replace(/\W/g, "");
+            }  
+        }
 
-    var storagePromise = browser.storage.local.get("rsidToGenotype");
-    storagePromise.then(function(data) { showResults(data.rsidToGenotype); }, console.log);
-};
-
-var tableChecker = setInterval(function() {
-    var table = document.querySelector(".variant-annotation-set table");
-    if (table !== null) {
-        clearInterval(tableChecker);
-        onTableLoad(table);
+        return rsidToGenotype;
     }
-}, 100);
+
+    var intervalHandle = null;
+
+    var intervalRun = function() {
+        if (intervalHandle != null) {
+            clearInterval(intervalHandle);
+        }
+
+        var storagePromise = browser.storage.local.get("rsidToGenotype");
+        storagePromise.then(function(data) {
+            var rsidToGenotype = JSON.parse(data.rsidToGenotype);
+            intervalHandle = setInterval(function() {
+                showResults(rsidToGenotype);
+            }, 1000);
+        }, function() {});
+    };
+
+    var resourceContent = new WeakSet();
+    setInterval(function() {
+        // Add an upload button to add genetic data
+        var content = document.querySelector(".resource-content");
+        if (!resourceContent.has(content)) {
+            resourceContent.add(content);
+            var div = document.createElement("div");
+            var title = document.createElement("h3");
+            title.innerText = "23andMe Data";
+            var clearLink = document.createElement("a");
+            clearLink.innerText = "Clear saved data";
+            var uploadButton = document.createElement("input");
+            uploadButton.setAttribute("type", "file");
+            div.appendChild(title);
+            div.appendChild(uploadButton);
+            div.appendChild(clearLink);
+            content.insertBefore(div, content.children[0]);
+
+            clearLink.addEventListener('click', function() {
+                browser.storage.local.remove("rsidToGenotype");
+                alert("Genetic data has been cleared");
+            });
+        
+            var onUpload = function(e1) {
+                var r = new FileReader();
+                r.onload = function(e2) {
+                    var contents = e2.target.result;
+                    var rsidToGenotype = parse23AndMe(contents);
+                    browser.storage.local.set({"rsidToGenotype": JSON.stringify(rsidToGenotype)});
+                    showResults(rsidToGenotype);
+                    intervalRun();
+                };
+                r.readAsText(e1.target.files[0]);
+            };
+            uploadButton.addEventListener("input", onUpload, false);
+        }
+    }, 1000);
+
+    intervalRun();
+})();
